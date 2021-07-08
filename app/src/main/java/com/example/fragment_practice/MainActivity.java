a3631e9d468be87224d192f23428fa22578337fe
myjeong <jeongm39@gmail.com>
2021-07-08 17:03:38 +0900

    item을 이용한 bottom navigation
    프래그먼트와 연결에서 문제 발생

--- app/src/main/java/com/example/fragment_practice/MainActivity.java
+++ app/src/main/java/com/example/fragment_practice/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.fragment_practice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //하던거
+    //https://choheeis.github.io/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C/2020/03/01/BottomNavigation.html
+
+    //하던거2
+    //https://itstudy-mary.tistory.com/190
+
+    //하던거3
+    //https://everybody-yeah.tistory.com/6
+
+
+    private BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        //첫 화면 띄우기
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_container,new Home()).commit();
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home_item:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new Home()).commit();
+                        break;
+                    case R.id.find_item:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new Find()).commit();
+                        break;
+                    case R.id.write_item:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new Write()).commit();
+                        break;
+                    case R.id.myPage_item:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new Mypage()).commit();
+                        break;
+
+                }
+
+
+                return true;
+            }
+        });
+
     }
 }
